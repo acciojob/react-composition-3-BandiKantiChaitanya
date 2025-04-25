929db8e8d3e14532d9bc0992fa27ccb256647deb
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-25 20:17:02 +0530

    updated version

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 
-import React, { useState } from "react";
+import React from "react";
 import './../styles/Tooltip.css';
 import Tooltip from "./Tooltip";
 
 const App = () => {
   
   return (
-    <div>
+    <div className="App" >
         <h2 className="tooltip">
           <Tooltip text="This is a tooltip for first element" >
           This is a tooltip
           </Tooltip>
         </h2>
-        <h2 className="tooltip">
+        <p className="tooltip">
           <Tooltip text="This is a tooltip for seccond element" >
           This is a another tooltip
           </Tooltip>
-        </h2>
+        </p>
     </div>
   )
 }
